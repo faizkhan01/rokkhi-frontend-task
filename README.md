2b2a5f26377e2de157eeaf1e43df4f8951c0044c
Md. Faizur Rahman Khan <khan.nayeem8662@gmail.com>
2020-12-28 22:01:56 +0600

    site coded successfully

---
+++ README.md
@@ -0,0 +1,1 @@
+# rokkhi-frontend-task
---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+// import './App.css';
+import Home from './Components/Home/Home';
+import SelectedCountry from './Components/Home/SelectedCountry';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+function App() {
+  return (
+    <div >
+ 
+      <Router>
+        
+        <Switch>
+          <Route exact path="/">
+             <Home></Home>
+          </Route>
+
+          <Route exact path="/selectedCountry">
+            <SelectedCountry></SelectedCountry>
+          </Route>
+
+          <Route path='*'>
+            <h2 className = 'text-center py-5'> 4O4  not found .......</h2>
+        </Route>
+        </Switch>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
